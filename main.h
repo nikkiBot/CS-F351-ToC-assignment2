d6adde48be10e82c7e8f14580fa4ae0c11ce53cf
nikkiBot <nishithmukka0208@gmail.com>
2022-12-09 16:38:28 +0530

    .

--- main.h
+++ main.h
@@ -5,6 +5,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 //structure to hold a node in syntax tree
 typedef struct Node {
@@ -17,6 +22,8 @@ typedef struct Node {
 void tokenize(char* filename) ;
 Node* create_node(char* value);
 void print_syntax_tree(Node* node, int depth);
-Node* parse_program(char* program, int num_tokens);
+Node* parse(char* program, int num_tokens);
+void checkFile(FILE* in_file);
+
 
 #endif
